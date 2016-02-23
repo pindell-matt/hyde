2ff652b88be3ea39d5e9ad9f56169133858cb922
Sal Espinosa <sespinos@gmail.com>
2016-02-23 14:58:00 -0700

    test: added @file_path

--- test/hyde_test.rb
+++ test/hyde_test.rb
@@ -13,20 +13,20 @@ require 'pry'
 class HydeTest < Minitest::Test
 
   def setup
-    file_path = File.expand_path('~/hyde-test')
-    FileUtils.rm_r(file_path) if File.exists? file_path
+    @file_path = File.expand_path('~/hyde-test')
+    FileUtils.rm_r(@file_path) if File.exists? @file_path
   end
 
   def test_new_creates_directory
-    file_path = '~/hyde-test'
-    submitted = `bin/hyde new #{file_path}`
+    # file_path = '~/hyde-test'
+    submitted = `bin/hyde new #{@file_path}`
 
-    assert File.exists? File.expand_path(file_path)
+    assert File.exists? File.expand_path(@file_path)
   end
 
   def test_auto_delete_is_working
-    file_path = '~/hyde-test'
-    refute File.exists? File.expand_path(file_path)
+    # file_path = '~/hyde-test'
+    refute File.exists? File.expand_path(@file_path)
   end
 
   # assert source is .md

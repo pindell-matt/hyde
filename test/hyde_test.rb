1b0e968c8c493aa09b3e144b18c56fd84f0dacd1
Sal Espinosa <sespinos@gmail.com>
2016-02-23 16:51:49 -0700

    test: html_uses_default_format

--- test/hyde_test.rb
+++ test/hyde_test.rb
@@ -18,7 +18,7 @@ class HydeTest < Minitest::Test
   end
 
   def test_new_creates_directory
-    submitted = `bin/hyde new #{@file_path}`
+    create = `bin/hyde new #{@file_path}`
 
     assert File.exists? File.expand_path(@file_path)
   end
@@ -28,25 +28,25 @@ class HydeTest < Minitest::Test
   end
 
   def test_that_main_css_is_created_in_source
-    submitted = `bin/hyde new #{@file_path}`
+    create = `bin/hyde new #{@file_path}`
 
     assert File.exists? File.expand_path(@file_path + '/source/css/main.css')
   end
 
   def test_that_index_markdown_is_created_in_source
-    submitted = `bin/hyde new #{@file_path}`
+    create = `bin/hyde new #{@file_path}`
 
     assert File.exists? File.expand_path(@file_path + '/source/index.markdown')
   end
 
   def test_that_about_markdown_is_created_in_source
-    submitted = `bin/hyde new #{@file_path}`
+    create = `bin/hyde new #{@file_path}`
 
     assert File.exists? File.expand_path(@file_path + '/source/pages/about.markdown')
   end
 
   def test_that_initial_post_markdown_is_created_in_source_with_timestamp
-    submitted = `bin/hyde new #{@file_path}`
+    create    = `bin/hyde new #{@file_path}`
     timestamp = Time.new.strftime('%Y-%m-%d') + '-'
 
     assert File.exists? File.expand_path(@file_path + '/source/posts/' + timestamp + 'welcome-to-hyde.markdown')
@@ -100,7 +100,20 @@ class HydeTest < Minitest::Test
     assert File.exists? File.expand_path(@file_path + '/_output/posts/' + file_name)
   end
 
+  def test_html_uses_default_format
+    post_name = "Default Formatted Post"
+    create    = `bin/hyde new #{@file_path}`
+    post      = `bin/hyde post #{@file_path} #{post_name}`
+    build     = `bin/hyde build #{@file_path}`
+    file_name = Time.new.strftime('%Y-%m-%d') + '-' + post_name.gsub(' ', '-') + '.html'
 
+    expected = "<body><div class='navbar'></div>
+    <h1 id=\"default-formatted-post\">Default Formatted Post</h1>
+    <p>Your content here</p><div class='footer'></div></body>\n"
+    actual = File.read File.expand_path(@file_path + '/_output/posts/' + file_name)
+
+    assert_equal expected.scan(/\S/).join, actual.scan(/\S/).join
+  end
 
   # file name matches ARGS
   # .md file has title content

c64cb9ef597987b746aa96eed13158f2206e372c
Sal Espinosa <sespinos@gmail.com>
2016-02-22 19:41:13 -0700

    updated parse_submission to use case loop

--- test/hyde_test.rb
+++ test/hyde_test.rb
@@ -1,14 +1,17 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
 # require 'simplecov'
 # SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
-require './bin/hyde'
+# require_relative '../bin/hyde'
+
 
 class HydeTest < Minitest::Test
 
   def test_args_contains_desired_action
-
+    # binding.pry
+    submitted = Hyde.new(build)
   end
 
 end

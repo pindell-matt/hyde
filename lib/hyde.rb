60cb7014f1441038ede8fc6fcd513ea6e98928cb
Sal Espinosa <sespinos@gmail.com>
2016-02-23 20:58:49 -0700

    Create generic file converter

--- lib/hyde.rb
+++ lib/hyde.rb
@@ -36,19 +36,32 @@ class Hyde
 
   def build_output
     FileUtils.cp_r(path + '/source/.', path + '/_output')
-    find_and_convert_markdown
-    find_and_convert_sass
+    # find_and_convert_markdown
+    # find_and_convert_sass
+    file_converter('markdown')
+    file_converter('sass')
+
   end
 
-  def find_and_convert_markdown
-    Dir.glob(path + '/_output' + '/**/*.markdown') do |md_file|
-      markdown_to_html(md_file)
-    end
-  end
+  # def find_and_convert_markdown
+  #   Dir.glob(path + '/_output' + '/**/*.markdown') do |md_file|
+  #     markdown_to_html(md_file)
+  #   end
+  # end
+  #
+  # def find_and_convert_sass
+  #   Dir.glob(path + '/_output' + '/**/*.sass') do |sass_file|
+  #     sass_to_css(sass_file)
+  #   end
+  # end
 
-  def find_and_convert_sass
-    Dir.glob(path + '/_output' + '/**/*.sass') do |sass_file|
-      sass_to_css(sass_file)
+  def file_converter(filetype)
+    Dir.glob(path + "/_output" + "/**/*.#{filetype}") do |file|
+      if filetype == "markdown"
+        markdown_to_html(file)
+      else
+        sass_to_css(file)
+      end
     end
   end
 
@@ -59,18 +72,17 @@ class Hyde
     File.rename(file, file.split('.')[0] + '.html')
   end
 
-  def sass_to_css(file)
-    sass = File.read(file)
-    sass_engine = Sass::Engine.new(sass)
-
-    File.open(file, 'w') { |file| file.write(sass_engine.render) }
-    File.rename(file, file.split('.')[0] + '.css')
-  end
-
   def default_html_format(markdown)
     html = Kramdown::Document.new(markdown).to_html
     template = File.read(path + '/source/layouts/default.html.erb')
     ERB.new(template).result(binding)
+  end
+
+  def sass_to_css(file)
+    sass = File.read(file)
+    sass_engine = Sass::Engine.new(sass)
+    File.open(file, 'w') { |file| file.write(sass_engine.render) }
+    File.rename(file, file.split('.')[0] + '.css')
   end
 
   def build_post

4429311f9490881b213b539764ad7eccb13c8171
Sal Espinosa <sespinos@gmail.com>
2016-02-24 22:19:00 -0700

    Update to insert all .css references into default template

--- test/hyde_integrations_test.rb
+++ test/hyde_integrations_test.rb
@@ -125,9 +125,10 @@ class HydeIntegrationsTest < Minitest::Test
     build     = `bin/hyde build #{@file_path}`
     file_name = Time.new.strftime('%Y-%m-%d') + '-' + post_name.gsub(' ', '-') + '.html'
 
-    expected = "<html>\n  <head><title>Our Site</title></head>\n
-      <body>\n    <h1 id=\"default-formatted-post\">Default Formatted Post</h1>\n
-      \n<p>Your content here</p>\n\n  </body>\n</html>\n"
+    expected = "<html><head><linkrel=\"stylesheet\"type=\"text/css\"href=\"
+    ./../css/main.css\"><linkrel=\"stylesheet\"type=\"text/css\"href=\"
+    ./../css/test.css\"><title>OurSite</title></head><body><h1id=\"default-formatted-post\">
+    DefaultFormattedPost</h1><p>Yourcontenthere</p></body></html>\n"
     actual = File.read File.expand_path(@file_path + '/_output/posts/' + file_name)
 
     assert_equal expected.scan(/\S/).join, actual.scan(/\S/).join
@@ -142,8 +143,10 @@ class HydeIntegrationsTest < Minitest::Test
     post    = `bin/hyde post #{@file_path} #{a_post}`
 
     file_name = Time.new.strftime('%Y-%m-%d') + '-' + a_post.gsub(' ', '-') + '.html'
-    expected  = "<html>\n  <head><title>Our Site</title></head>\n
-    <body>\n    <h1 id=\"initial-post\">Initial Post</h1>\n\n<p>Your content here</p>\n\n  </body>\n</html>\n"
+    expected  = "<html><head><linkrel=\"stylesheet\"type=\"text/css\"href=\"
+    ./../css/main.css\"><linkrel=\"stylesheet\"type=\"text/css\"href=\"
+    ./../css/test.css\"><title>OurSite</title></head><body><h1id=\"initial-post\">
+    InitialPost</h1><p>Yourcontenthere</p></body></html>\n"
     pause     = sleep(1)
     actual    = File.read File.expand_path(@file_path + '/_output/posts/' + file_name)
 

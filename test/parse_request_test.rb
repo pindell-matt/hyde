25c398799d963515d3fd386e1c382b6ed1185216
Matt Pindell <pindell.m@gmail.com>
2016-02-24 21:13:32 -0700

    test: raise argument error for duplicate new directory

--- test/parse_request_test.rb
+++ test/parse_request_test.rb
@@ -8,23 +8,36 @@ require_relative '../lib/parse_request'
 
 class ParseRequestTest < Minitest::Test
 
+  def setup
+    @working_directory = Dir.pwd
+    @file_path = File.expand_path(@working_directory + '/test/test-support')
+    FileUtils.rm_r(@file_path) if File.exists? @file_path
+  end
+
   def test_parse_request_can_be_created
-    submitted = ParseRequest.new('action', 'path', 'title')
+    submitted = ParseRequest.new('action', @file_path, 'title')
 
     assert_kind_of ParseRequest, submitted
   end
 
   def test_parse_request_has_three_ivars
-    submitted = ParseRequest.new('action', 'path', 'title')
+    submitted = ParseRequest.new('action', @file_path, 'title')
 
     assert_equal 'action', submitted.action
-    assert_equal 'path', submitted.path
+    assert_equal @file_path, submitted.path
     assert_equal 'title', submitted.title
   end
 
   def test_parse_submission_raises_arg_error_with_invalid_input
     assert_raises ArgumentError do
-      ParseRequest.new('invalid', 'path', 'title').parse_submission
+      ParseRequest.new('invalid', @file_path, 'title').parse_submission
+    end
+  end
+
+  def test_parse_request_raises_arg_error_if_directory_already_exists
+    submitted = ParseRequest.new('new', @file_path, 'title').parse_submission
+    assert_raises ArgumentError do
+      ParseRequest.new('invalid', @file_path, 'title').parse_submission
     end
   end
 

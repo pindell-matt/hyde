2270df0f116c6bd29a6f5ac1a03b3a90b569f0b8
Matt Pindell <pindell.m@gmail.com>
2016-02-25 13:37:24 -0700

    test: Post tests

---
+++ test/post_test.rb
@@ -0,0 +1,31 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+require_relative '../lib/post'
+require_relative '../lib/generate_new'
+
+class PostTest < Minitest::Test
+  attr_reader :build_path, :test_path
+
+  def setup
+    @working_directory = Dir.pwd
+    @build_path = File.expand_path(@working_directory + '/test/test_content/building')
+    @test_path = File.expand_path(@working_directory + '/test/test_content/test_dir')
+    FileUtils.rm_r(build_path) if File.exists? (build_path)
+  end
+
+  def test_post_can_build_post
+    name = "Test"
+    hyde = GenerateNew.new(build_path)
+    hyde.build_new
+
+    post = Post.new(build_path, [name]).build_post
+    time = Time.new.strftime('%Y-%m-%d') + '-' + name
+
+    assert File.exists? File.expand_path(build_path + "/source/posts/#{time}.markdown")
+  end
+
+end
